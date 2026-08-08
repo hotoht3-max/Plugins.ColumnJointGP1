5b659f6bbe0c4a901ed4fa9ea10967cb5e72113d
HYPERPC <HYPERPC@DESKTOP-51M9194>
2026-08-08 20:44:52 +0300

    Alpha 1.05

---
+++ UIControls/GussetShapeControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace RAM.Plugins.ColumnJointGP1.UIControls
+{
+    public partial class GussetShapeControl : UserControl
+    {
+        public GussetShapeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
